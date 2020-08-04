5b4239507a9549f6b9cb2015de2ea6b206fb59a9
rpingat <pingatravi@gmail.com>
2020-08-04 07:52:33 +0200

    scripts: make some changes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Export some variables
 user=
 OUT_PATH="out/target/product/$device"
-ROM_ZIP="AIM*.zip"
+tg_username= 
 
 # Export colors
 export TERM=xterm
@@ -13,7 +13,17 @@ export TERM=xterm
     blu=$(tput setaf 4) # blue
     cya=$(tput setaf 6) # cyan
     txtrst=$(tput sgr0) # Reset
+# Send message to TG
+read -r -d '' msg <<EOT
+<b>Build Started</b>
 
+<b>Device:-</b> ${device}
+<b>Job Number:-</b> ${BUILD_NUMBER}
+<b>Started by:-</b> ${tg_username}
+
+Check progress <a href="${BUILD_URL}console">HERE</a>
+EOT
+telegram-send --format html "$msg"
 # Export ccache
 if [ "$use_ccache" = "yes" ];
 then
@@ -49,12 +59,37 @@ fi
 if [ "$make_clean" = "installclean" ];
 then
 make installclean
-rm -rf $OUT_PATH/$ROM_ZIP
+rm -rf ${OUT_PATH}/AIM*.zip
 wait
 echo -e ${cya}"Images deleted from OUT dir"${txtrst};
 fi
 
 #Time to build
 source build/envsetup.sh
-lunch aim_${device}-"$build_type"
+lunch aim_${device}-"${build_type}"
 make bacon -j32
+
+# Build status
+if [ `ls ${OUT_PATH}/AIM*.zip 2>/dev/null | wc -l` != "0" ]; then
+read -r -d '' msg1 <<EOT
+<b>Build Finished</b>
+
+<b>Device:-</b> ${device}
+<b>Build status:-</b> Success
+<b>Started by:-</b> ${tg_username}
+
+Check console output <a href="${BUILD_URL}console">HERE</a>
+EOT
+telegram-send --format html "$msg1"
+else
+read -r -d '' msg1 <<EOT
+<b>Build Finished</b>
+
+<b>Device:-</b> ${device}
+<b>Build status:-</b> Failed
+<b>Started by:-</b> ${tg_username}
+
+Check what caused your build to fail <a href="${BUILD_URL}console">HERE</a>
+EOT
+telegram-send --format html "$msg1"
+fi
