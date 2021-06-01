a58fcf777477ac15a1cb60137bdd8b25c79c6941
Ravi Pingat <pingatravi@gmail.com>
2021-06-01 22:35:08 +0530

    Update script_build.sh

--- script_build.sh
+++ script_build.sh
@@ -1,28 +1,67 @@
 #!/usr/bin/env bash
 
-user=
+# curl https://raw.githubusercontent.com/Rpingat/scripts/master/script_build.sh>script_build.sh
+# Make necessary changes before executing script
 
-# Export ccache
+# Export some variables
+user=
+device_codename=
+OUT_PATH="out/target/product/$device_codename"
+ROM_ZIP=Rom*.zip
+
+# Colors makes things beautiful
+export TERM=xterm
+
+    red=$(tput setaf 1)             #  red
+    grn=$(tput setaf 2)             #  green
+    blu=$(tput setaf 4)             #  blue
+    cya=$(tput setaf 6)             #  cyan
+    txtrst=$(tput sgr0)             #  Reset
+
+# Ccache
+if [ "$use_ccache" = "yes" ];
+then
+echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export CCACHE_COMPRESS=1
 export CCACHE_DIR=/home/$user/ccache
-ccache -M 75G
+ccache -M 50G
+fi
 
-#Clear ccache
-#export CCACHE_EXEC=$(which ccache)
-#export CCACHE_DIR=/path/to/ccache
-#ccache -C
-#export USE_CCACHE=1
-#export CCACHE_COMPRESS=1
-#ccache -M 75G
+if [ "$use_ccache" = "clean" ];
+then
+export CCACHE_EXEC=$(which ccache)
+export CCACHE_DIR=/home/$user/ccache
+ccache -C
+export USE_CCACHE=1
+ccache -M 50G
+wait
+echo -e ${grn}"CCACHE Cleared"${txtrst};
+fi
 
-#Export User and host variables
-export KBUILD_BUILD_USER=$user
-export KBUILD_BUILD_HOST=ThunderServer
+rm -rf ${OUT_PATH}/${ROM_ZIP} #clean rom zip in any case
 
-#Time to 
+# Time to build
 source build/envsetup.sh
+# Make clean
+if [ "$make_clean" = "yes" ];
+then
 make clean
-lunch lineage_device-userdebug
+wait
+echo -e ${cya}"OUT dir from your repo deleted"${txtrst};
+fi
+
+if [ "$make_clean" = "installclean" ];
+then
+make installclean
+wait
+echo -e ${cya}"Images deleted from OUT dir"${txtrst};
+fi
+lunch lineage_"$device_codename"-userdebug
 make bacon -j16
+
+if [ `ls $OUT_PATH/$ROM_ZIP 2>/dev/null | wc -l` != "0" ]; then
+cd $OUT_PATH
+RZIP="$(ls ${ROM_ZIP})"
+cp ${RZIP} /home/ravi/$user
+fi
