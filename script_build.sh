66c4af4264557169b8bd73383cb5a80379ba01ad
rpingat <pingatravi@gmail.com>
2021-07-02 17:14:21 +0200

    update script

--- script_build.sh
+++ script_build.sh
@@ -10,6 +10,18 @@ lunch=
 build_type=
 OUT_PATH="out/target/product/$device_codename"
 ROM_ZIP=*.zip
+tg_username=@
+
+read -r -d '' msg <<EOT
+<b>Build Started</b>
+
+<b>Device:-</b> ${device_codename}
+<b>Job Number:-</b> ${BUILD_NUMBER}
+<b>Started by:-</b> ${tg_username}
+
+Check progress <a href="${BUILD_URL}console">HERE</a>
+EOT
+telegram-send --format html "$msg"
 
 # Ccache
 if [ "$use_ccache" = "yes" ];
@@ -47,12 +59,43 @@ make installclean
 wait
 fi
 lunch "$lunch"_"$device_codename"-"$build_type"
+START=$(date +%s)
 make bacon -j24
+END=$(date +%s)
+TIME=$(echo $((${END}-${START})) | awk '{print int($1/60)" Minutes and "int($1%60)" Seconds"}')
 
 if [ `ls $OUT_PATH/$ROM_ZIP 2>/dev/null | wc -l` != "0" ]; then
 cd $OUT_PATH
 RZIP="$(ls ${ROM_ZIP})"
 cp ${RZIP} /home/ravi/builds/${user}
+link="https://mirror.thunderserver.in/$user/$RZIP"
+read -r -d '' suc <<EOT
+<b>Build Finished</b>
+
+<b>Time:-</b> ${TIME}
+<b>Device:-</b> ${device_codename}
+<b>Build status:-</b> Success
+<b>Download:-</b> <a href="${link}">$RZIP</a>
+
+Check console output <a href="${BUILD_URL}console">HERE</a>
+
+cc: ${tg_username}
+EOT
+telegram-send --format html "$suc"
 else
+
+read -r -d '' fail <<EOT
+<b>Build Finished</b>
+
+<b>Time:-</b> ${TIME}
+<b>Device:-</b> ${device_codename}
+<b>Build status:-</b> Failed
+
+Check what caused build to fail <a href="${BUILD_URL}console">HERE</a>
+
+cc: ${tg_username}
+EOT
+telegram-send --format html "$fail"
+
 exit 1
 fi
